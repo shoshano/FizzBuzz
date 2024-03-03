82c60748aa54a756674e16c9f2795b873e42a186
shoshano <shoshano2048@gmail.com>
2024-03-03 16:02:13 +0100

    Dodaj pliki projektów.

---
+++ Projekt1_FizzBuzz/FizzBuzz_game.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1_FizzBuzz
+{
+    internal class FizzBuzz_game
+    {
+        public int upper_range;
+        public FizzBuzz_game(int ur) 
+        {
+            upper_range = ur;
+            print_results();
+        }
+
+        public string result(int x)
+        {
+            string answer = "";
+            if (x % 3 == 0)
+            {
+                answer = answer + "Fizz";            
+            }
+            if (x % 5 == 0)
+            {
+                answer = answer + "Buzz";            
+            }
+            if (answer == "")
+            {
+                answer = Convert.ToString(x);
+            }
+            return answer;
+        }
+        public void print_results()
+        {
+            for (int i = 1; i < upper_range + 1; i++)
+            {
+                string  answer = result(i);
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
---
+++ Projekt1_FizzBuzz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projekt1_FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj gorny zakres: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            FizzBuzz_game mygame = new FizzBuzz_game(number);
+        }
+    }
+}
